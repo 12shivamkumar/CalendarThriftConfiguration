9a0e3c798c3a1f27a52359b0a0e42de14c198b99
tusharmelmalgi <tushar.melmalgi@capillarytech.com>
2022-07-29 10:59:55 +0530

    meeting details struct created ;

--- src/main/thrift/CalendarMeetingService.thrift
+++ src/main/thrift/CalendarMeetingService.thrift
@@ -22,15 +22,16 @@ struct MeetingDetails{
     9:i32 roomId;
 }
 struct EmployeeMeetingDetails{
-    1:string status;
-    2:string description;
-    3:string agenda;
-    4:string ownerId;
-    5:Date dateOfMeeting;
-    6:Time startTime;
-    7:Time endTime;
-    8:bool isAvailable;
-    9:i32 roomId;
+    1:i32 meetId;
+    2:string status;
+    3:string description;
+    4:string agenda;
+    5:string ownerId;
+    6:Date dateOfMeeting;
+    7:Time startTime;
+    8:Time endTime;
+    9:bool isAvailable;
+    10:i32 roomId;
 }
 struct EmployeeStatusDataRequest {
     1:string employeeId;
@@ -66,7 +67,7 @@ service MeetingSvc
 
    list<string> checkEmployeeAvailability(1:EmployeeAvailabilityDataRequest employeeDetails);
 
-   string addMeetingDetails(1:MeetingDetails meetingDetails);
+   i32 addMeetingDetails(1:MeetingDetails meetingDetails);
 
    i32 findFreeMeetingRoom(1:FindFreeMeetingRoomDataRequest findFreeMeetingRoomDataRequest);
 
